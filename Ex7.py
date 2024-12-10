341bb29e5c7f3339cdeb9230758d99c8b30fc1f5
Arthur <arthurberthomme1@gmail.com>
2024-12-10 11:50:00 +0100

    message

---
+++ Ex7.py
@@ -0,0 +1,5 @@
+chaine = input("Ecrivez une chaine de caractere :")
+if 'a' in chaine:
+    print("Votre chaine de caractere contient un a")
+else:
+    print("Votre chaine de caractere ne contient pas la lettre a")
